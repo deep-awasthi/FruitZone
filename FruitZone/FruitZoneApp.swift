a0699b811a8ed5edd9c6b8459ed98757bff3ddc1
deep-awasthi <da.madskull@gmail.com>
2024-05-21 18:53:07 +0530

    Complete App

---
+++ FruitZone/FruitZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitZoneApp.swift
+//  FruitZone
+//
+//  Created by Deep Awasthi on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FruitZone/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  FruitZone
+//
+//  Created by Deep Awasthi on 21/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        OnboardingScreenView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
